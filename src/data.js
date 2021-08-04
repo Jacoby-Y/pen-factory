1d9a6162ce6b39da0f4a4a22989523336b53659a
Jacoby Yliniemi <cobyyliniemi@gmail.com>
2021-08-04 13:31:56 -0500

    Made idle information more accurate

--- src/data.js
+++ src/data.js
@@ -1,3 +1,7 @@
+data.settings = [
+    {k: "idle_money_ps", display(val){return data.money_pp * data.idle_craft_ps}}
+];
+
 data.material = 0;
 data.ink = 0;
 data.money = 1000;
@@ -6,19 +10,36 @@ data.material_pc = 1;
 data.ink_pc = 1;
 data.craft_pc = 1;
 
-data.material_mult = 0;
-data.multiplier_ink = 0;
-data.multiplier_craft = 0;
-
-data.idle_material_mult = 0;
-data.idle_multiplier_ink = 0;
-data.idle_multiplier_craft = 0;
+data.idle_material_ps = 0;
+data.idle_ink_ps = 0;
+data.idle_craft_ps = 0;
 
 data.money_pp = 1;
 data.material_pp = 10;
 data.ink_pp = 10;
 
-data.cost_upgrade_mat_pc = 10//At the moment those values are not changing but will later 
-data.cost_upgrade_ink_pc = 10
-data.cost_upgrade_craft_pc = 10
-data.cost_upgrade_money_pp = 100
+data.idle_money_ps = 0;
+
+data.cost_upgrade_mat_pc = 10;
+data.cost_upgrade_ink_pc = 10;
+data.cost_upgrade_craft_pc = 10;
+data.cost_upgrade_money_pp = 100;
+
+data.cost_upgrade_mat_idle = 10;
+data.cost_upgrade_ink_idle = 10;
+data.cost_upgrade_craft_idle = 10;
+
+//#region Dom Constants
+const get_material = $("#get-material")[0];
+const get_ink = $("#get-ink")[0];
+const craft_sell = $("#craft-sell")[0];
+const craft_infos = $(".craft-info");
+craft_infos.css("display", "none");
+const upgrade_mat = $("#upgrade-mat-pc")[0];
+const upgrade_ink = $("#upgrade-ink-pc")[0];
+const upgrade_craft = $("#upgrade-craft-pc")[0];
+const upgrade_money_pp = $("#upgrade-money-pp")[0];
+const upgrade_idle_mat = $("#upgrade-idle-material")[0];
+const upgrade_idle_ink = $("#upgrade-idle-ink")[0];
+const upgrade_idle_craft = $("#upgrade-idle-craft")[0];
+//#endregion
--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
-const get_material = $("#get-material")[0];
-const get_ink = $("#get-ink")[0];
-const craft_sell = $("#craft-sell")[0];
-const craft_infos = $(".craft-info");
-craft_infos.css("display", "none");
-// console.log(craft_infos);
-const upgrade_mat = $("#upgrade-mat-pc")[0]
-const upgrade_ink = $("#upgrade-ink-pc")[0]
-const upgrade_craft = $("#upgrade-craft-pc")[0]
-const upgrade_money_pp = $("#upgrade-money-pp")[0]
 get_material.onclick = ()=>{
     data.material = data.material + data.material_pc;
 }
@@ -23,7 +13,7 @@ craft_sell.onclick = () => {
     const craft_pc = data.craft_pc;
     let i = 0
     while (material >= material_per && ink >= ink_per && i < craft_pc) {
-        console.log(money)
+        // console.log(money)
         money = money + data.money_pp ;
         material -= material_per;
         ink -= ink_per;
@@ -39,6 +29,14 @@ const max_pens_pc = ()=>{
     const max_ink = Math.floor(data.ink / data.ink_pp);
     return Math.min(max_material, max_ink);
 }
+data.settings.push({k: "idle_craft_ps", display(val){
+    const max_pens = max_pens_pc();
+    let current = 0;
+    if (max_pens > val) current = val;
+    else current = max_pens;
+    return `${current} pen/sec\nMax: ${val}`;
+}});
+data.idle_craft_ps = data.idle_craft_ps;
 
 craft_sell.onmouseenter = ()=>{
     craft_infos.css("display", "block");
@@ -57,27 +55,73 @@ craft_sell.onmouseleave = ()=>{
     craft_infos.css("display", "none");
 }
 
+upgrade_idle_mat.onmouseenter = ()=>{ upgrade_idle_mat.lastElementChild.style.display = "block"; }
+upgrade_idle_mat.onmouseleave = ()=>{ upgrade_idle_mat.lastElementChild.style.display = "none"; }
+
+upgrade_idle_ink.onmouseenter = ()=>{ upgrade_idle_ink.lastElementChild.style.display = "block"; }
+upgrade_idle_ink.onmouseleave = ()=>{ upgrade_idle_ink.lastElementChild.style.display = "none"; }
+
+upgrade_idle_craft.onmouseenter = ()=>{ upgrade_idle_craft.lastElementChild.style.display = "block"; }
+upgrade_idle_craft.onmouseleave = ()=>{ upgrade_idle_craft.lastElementChild.style.display = "none"; }
+
 upgrade_mat.onclick = ()=>{
     if (data.money >= data.cost_upgrade_mat_pc){
         data.money -=data.cost_upgrade_mat_pc;
-        data.material_pc +=1;
+        data.material_pc++;
     }
 }
 upgrade_ink.onclick = ()=>{
     if (data.money >= data.cost_upgrade_ink_pc){
         data.money -=data.cost_upgrade_ink_pc;
-        data.ink_pc +=1;
+        data.ink_pc++;
     }
 }
 upgrade_craft.onclick = ()=>{
     if (data.money >= data.cost_upgrade_craft_pc){
         data.money -=data.cost_upgrade_craft_pc;
-        data.craft_pc +=1;
+        data.craft_pc++;
     }
 }
 upgrade_money_pp.onclick = ()=>{
     if (data.money >= data.cost_upgrade_money_pp){
         data.money -=data.cost_upgrade_money_pp;
-        data.money_pp +=1;
+        data.money_pp++;
     }
 }
+upgrade_idle_mat.onclick = () => {
+    if (data.money >= data.cost_upgrade_mat_idle){
+        data.money -= data.cost_upgrade_mat_idle;
+        data.idle_material_ps++;
+    }
+}
+upgrade_idle_ink.onclick = () => {
+    if (data.money >= data.cost_upgrade_ink_idle){
+        data.money -= data.cost_upgrade_ink_idle;
+        data.idle_ink_ps++;
+    }
+}
+upgrade_idle_craft.onclick = () => {
+    if (data.money >= data.cost_upgrade_craft_idle){
+        data.money -= data.cost_upgrade_craft_idle;
+        data.idle_craft_ps++;
+    }
+}
+
+const idle_loop = setInterval(() => {
+    const max_pen = max_pens_pc();
+    if (data.idle_material_ps > 0) data.material += data.idle_material_ps
+    if (data.idle_ink_ps > 0) data.ink += data.idle_ink_ps
+    if (data.idle_craft_ps <= 0) return;
+
+    if (data.idle_craft_ps < max_pen ){
+        data.money += data.idle_craft_ps * data.money_pp
+        data.material -= data.idle_craft_ps * data.material_pp
+        data.ink -= data.idle_craft_ps * data.ink_pp
+        data.idle_craft_ps = data.idle_craft_ps;
+    }else{
+        data.money += max_pen* data.money_pp 
+        data.material -= max_pen * data.material_pp
+        data.ink -= max_pen * data.ink_pp
+        data.idle_craft_ps = data.idle_craft_ps;
+    }
+}, 1000);
