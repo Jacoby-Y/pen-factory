91843f235e18a0c1ddf26f5fbfd0173048a036b8
Jacoby Yliniemi <cobyyliniemi@gmail.com>
2021-08-04 15:11:02 -0500

    v1.0 [alpha]

--- src/data.js
+++ src/data.js
@@ -1,40 +1,79 @@
-data.settings = [
-    {k: "idle_money_ps", display(val){return data.money_pp * data.idle_craft_ps}}
+const max_pens_pc = ()=>{
+    const max_material = Math.floor(data.material / data.material_pp);
+    const max_ink = Math.floor(data.ink / data.ink_pp);
+    return Math.min(max_material, max_ink);
+}
+
+settings = [
+    {k:"idle_money_ps", display(val){return data.money_pp * data.idle_craft_ps}},
+    {k:"money", display(val){return Math.round(val*10)/10}},
+    {k:"cost_upgrade_mat_pc", display(val){return Math.round(val*10)/10}},
+    {k:"cost_upgrade_ink_pc", display(val){return Math.round(val*10)/10}},
+    {k:"cost_upgrade_craft_pc", display(val){return Math.round(val*10)/10}},
+    {k:"cost_upgrade_money_pp", display(val){return Math.round(val*10)/10}},
+    {k:"cost_upgrade_mat_idle", display(val){return Math.round(val*10)/10}},
+    {k:"cost_upgrade_ink_idle", display(val){return Math.round(val*10)/10}},
+    {k:"cost_upgrade_craft_idle", display(val){return Math.round(val*10)/10}},
+    {k:"cost_prestige_material", display(val){return Math.round(val*10)/10}},
+    {k:"cost_prestige_ink", display(val){return Math.round(val*10)/10}},
+    {k: "idle_craft_ps", display(val){
+        const max_pens = max_pens_pc();
+        let current = 0;
+        if (max_pens > val) current = val;
+        else current = max_pens;
+        return `${current} pen/sec\nMax: ${val}`;
+    }}
 ];
 
-data.material = 0;
-data.ink = 0;
-data.money = 1000;
+if (local.can_load()) {
+    // local.clear();
+    // console.log(`Ink: ${local.load().ink}`);
+    data = local.load();
+    data.settings = settings;
+    local.finish();
+    // console.log(`Ink: ${local.load().ink}`);
+} else {
+    data.settings = settings;
 
-data.material_pc = 1;
-data.ink_pc = 1;
-data.craft_pc = 1;
-
-data.idle_material_ps = 0;
-data.idle_ink_ps = 0;
-data.idle_craft_ps = 0;
-
-data.money_pp = 1;
-data.material_pp = 10;
-data.ink_pp = 10;
-
-data.idle_money_ps = 0;
-
-data.cost_upgrade_mat_pc = 10;
-data.cost_upgrade_ink_pc = 10;
-data.cost_upgrade_craft_pc = 10;
-data.cost_upgrade_money_pp = 100;
-
-data.cost_upgrade_mat_idle = 10;
-data.cost_upgrade_ink_idle = 10;
-data.cost_upgrade_craft_idle = 10;
-
+    data.material = 0;
+    data.ink = 0;
+    data.money = 200000;
+    
+    data.material_pc = 1;
+    data.ink_pc = 1;
+    data.craft_pc = 1;
+    
+    data.idle_material_ps = 0;
+    data.idle_ink_ps = 0;
+    data.idle_craft_ps = 0;
+    
+    data.money_pp = 1;
+    data.material_pp = 10;
+    data.ink_pp = 10;
+    
+    data.idle_money_ps = 0;
+    
+    data.cost_upgrade_mat_pc = 10;
+    data.cost_upgrade_ink_pc = 10;
+    data.cost_upgrade_craft_pc = 10;
+    data.cost_upgrade_money_pp = 100;
+    
+    data.cost_upgrade_mat_idle = 10;
+    data.cost_upgrade_ink_idle = 10;
+    data.cost_upgrade_craft_idle = 10;
+    
+    data.cost_prestige_material = 1000;
+    data.cost_prestige_ink = 1000;
+    
+    data.can_win = false;
+    data.win_game_cost = 100000;
+}
+    
 //#region Dom Constants
 const get_material = $("#get-material")[0];
 const get_ink = $("#get-ink")[0];
 const craft_sell = $("#craft-sell")[0];
 const craft_infos = $(".craft-info");
-craft_infos.css("display", "none");
 const upgrade_mat = $("#upgrade-mat-pc")[0];
 const upgrade_ink = $("#upgrade-ink-pc")[0];
 const upgrade_craft = $("#upgrade-craft-pc")[0];
@@ -42,4 +81,7 @@ const upgrade_money_pp = $("#upgrade-money-pp")[0];
 const upgrade_idle_mat = $("#upgrade-idle-material")[0];
 const upgrade_idle_ink = $("#upgrade-idle-ink")[0];
 const upgrade_idle_craft = $("#upgrade-idle-craft")[0];
+const reduce_material = $("#reduce-material")[0];
+const reduce_ink = $("#reduce-ink")[0];
+const win_game = $("#win-game")[0];
 //#endregion
--- src/main.js
+++ src/main.js
@@ -24,90 +24,71 @@ craft_sell.onclick = () => {
     data.money = money;
 }
 
-const max_pens_pc = ()=>{
-    const max_material = Math.floor(data.material / data.material_pp);
-    const max_ink = Math.floor(data.ink / data.ink_pp);
-    return Math.min(max_material, max_ink);
-}
-data.settings.push({k: "idle_craft_ps", display(val){
-    const max_pens = max_pens_pc();
-    let current = 0;
-    if (max_pens > val) current = val;
-    else current = max_pens;
-    return `${current} pen/sec\nMax: ${val}`;
-}});
 data.idle_craft_ps = data.idle_craft_ps;
 
-craft_sell.onmouseenter = ()=>{
-    craft_infos.css("display", "block");
-    const max_pens = max_pens_pc();
-    if (max_pens > 0) {
-        craft_infos.css("color", "green");
-        craft_infos[0].innerText = `-${data.material_pp*max_pens}`;
-        craft_infos[1].innerText = `-${data.ink_pp*max_pens}`;
-    } else {
-        craft_infos.css("color", "red");
-        craft_infos[0].innerText = `-${data.material_pp}`;
-        craft_infos[1].innerText = `-${data.ink_pp}`;
-    }
-}
-craft_sell.onmouseleave = ()=>{
-    craft_infos.css("display", "none");
-}
+// upgrade_idle_mat.onmouseenter = ()=>{ upgrade_idle_mat.lastElementChild.style.display = "block"; }
+// upgrade_idle_mat.onmouseleave = ()=>{ upgrade_idle_mat.lastElementChild.style.display = "none"; }
 
-upgrade_idle_mat.onmouseenter = ()=>{ upgrade_idle_mat.lastElementChild.style.display = "block"; }
-upgrade_idle_mat.onmouseleave = ()=>{ upgrade_idle_mat.lastElementChild.style.display = "none"; }
+// upgrade_idle_ink.onmouseenter = ()=>{ upgrade_idle_ink.lastElementChild.style.display = "block"; }
+// upgrade_idle_ink.onmouseleave = ()=>{ upgrade_idle_ink.lastElementChild.style.display = "none"; }
 
-upgrade_idle_ink.onmouseenter = ()=>{ upgrade_idle_ink.lastElementChild.style.display = "block"; }
-upgrade_idle_ink.onmouseleave = ()=>{ upgrade_idle_ink.lastElementChild.style.display = "none"; }
-
-upgrade_idle_craft.onmouseenter = ()=>{ upgrade_idle_craft.lastElementChild.style.display = "block"; }
-upgrade_idle_craft.onmouseleave = ()=>{ upgrade_idle_craft.lastElementChild.style.display = "none"; }
+// upgrade_idle_craft.onmouseenter = ()=>{ upgrade_idle_craft.lastElementChild.style.display = "block"; }
+// upgrade_idle_craft.onmouseleave = ()=>{ upgrade_idle_craft.lastElementChild.style.display = "none"; }
 
 upgrade_mat.onclick = ()=>{
     if (data.money >= data.cost_upgrade_mat_pc){
         data.money -=data.cost_upgrade_mat_pc;
+        data.cost_upgrade_mat_pc *= 1.5;
         data.material_pc++;
     }
 }
 upgrade_ink.onclick = ()=>{
     if (data.money >= data.cost_upgrade_ink_pc){
-        data.money -=data.cost_upgrade_ink_pc;
+        data.money -= data.cost_upgrade_ink_pc;
+        data.cost_upgrade_ink_pc *= 1.5;
         data.ink_pc++;
     }
 }
 upgrade_craft.onclick = ()=>{
     if (data.money >= data.cost_upgrade_craft_pc){
         data.money -=data.cost_upgrade_craft_pc;
+        data.cost_upgrade_craft_pc *= 1.5;
         data.craft_pc++;
     }
 }
 upgrade_money_pp.onclick = ()=>{
     if (data.money >= data.cost_upgrade_money_pp){
         data.money -=data.cost_upgrade_money_pp;
+        data.cost_upgrade_money_pp *= 1.2;
         data.money_pp++;
     }
 }
 upgrade_idle_mat.onclick = () => {
     if (data.money >= data.cost_upgrade_mat_idle){
         data.money -= data.cost_upgrade_mat_idle;
+        data.cost_upgrade_mat_idle *= 1.5;
         data.idle_material_ps++;
     }
 }
 upgrade_idle_ink.onclick = () => {
     if (data.money >= data.cost_upgrade_ink_idle){
         data.money -= data.cost_upgrade_ink_idle;
+        data.cost_upgrade_ink_idle *= 1.5;
         data.idle_ink_ps++;
     }
 }
 upgrade_idle_craft.onclick = () => {
     if (data.money >= data.cost_upgrade_craft_idle){
         data.money -= data.cost_upgrade_craft_idle;
+        data.cost_upgrade_craft_idle *= 1.5;
         data.idle_craft_ps++;
     }
 }
 
 const idle_loop = setInterval(() => {
+    local.store();
+    // console.log(local.load().ink);
+
     const max_pen = max_pens_pc();
     if (data.idle_material_ps > 0) data.material += data.idle_material_ps
     if (data.idle_ink_ps > 0) data.ink += data.idle_ink_ps
@@ -125,3 +106,39 @@ const idle_loop = setInterval(() => {
         data.idle_craft_ps = data.idle_craft_ps;
     }
 }, 1000);
+
+reduce_ink.onclick = () =>{
+    if(data.ink_pp > 1 && data.money >= data.cost_prestige_ink  ){
+        data.money -= data.cost_prestige_ink;
+        data.cost_prestige_ink *= 1.25;
+        data.ink_pp -= 1;
+    } 
+    if (data.ink_pp <= 1) {
+        reduce_ink.innerHTML = "<h3>*Maxed out*</h3>"
+        if(data.material_pp <= 1){
+            $("#win-game")[0].innerHTML = "<h3>Start a monopoly</h3><h4 watch>Cost: {win_game_cost}</h4>";
+            data.win_game_cost = data.win_game_cost;
+            data.can_win = true;
+        }
+    }
+}
+reduce_material.onclick = () =>{
+    if(data.material_pp >1 && data.money >= data.cost_prestige_material  ){
+        data.money -= data.cost_prestige_material;
+        data.cost_prestige_material *= 1.25;
+        data.material_pp -=1;
+    } 
+    if (data.material_pp <= 1) {
+        reduce_material.innerHTML = "<h3>*Maxed out*</h3>";
+        if(data.ink_pp <= 1){
+            $("#win-game")[0].innerHTML = "<h3>Start a monopoly</h3><h4 watch>Cost: {win_game_cost}</h4>";
+            data.win_game_cost = data.win_game_cost;
+            data.can_win = true;
+        }
+    }
+}
+$("#win-game")[0].onclick = ()=>{
+    if (data.can_win && data.money >= data.win_game_cost){
+        alert("After all these years, you finally have a monopoly over the pen industry. You pens will be remembered for the next thousand years!");
+    }
+}
